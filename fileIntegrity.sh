b47e13192ce2d7e86a51cfeab1f6ef92bedaafeb
7CLewis <casey.lewis08@gmail.com>
2020-02-10 19:15:17 -0700

    Update fileIntegrity.sh

--- fileIntegrity.sh
+++ fileIntegrity.sh
@@ -5,66 +5,44 @@
 # you should delete the sums.txt file and execute this script immediately
 # after the deletion (You should also check the log file to verify that
 # no other files were unintentionally/maliciously modified before you).
-FILE=/example/dir/logging/sums.txt
-if [ ! -f "$FILE" ]; then
-    echo -e "MD5 Checksums for /home/user/Cyber/sums:\n" > /example/dir/logging/sums.txt
-    for i in /example/dir/dir-to-check/*
+SUMFILE=/home/user/Cyber/sums.txt
+if [ ! -f "$SUMFILE" ]; then
+    echo -e "MD5 Checksums for /home/user/Cyber/sums:\n" > $SUMFILE
+    for i in /home/user/Cyber/sums/*
     do
-        echo "File Name: " $i >> /example/dir/logging/sums.txt
-        md5sum $i | awk ' { print "MD5 Sum: " $1 } ' >> /example/dir/logging/sums.txt
-        echo "" >> /example/dir/logging/sums.txt
+        echo "File Name: " $i >> $SUMFILE
+        md5sum $i | awk ' { print "MD5 Sum: " $1 } ' >> $SUMFILE
+        echo "" >> $SUMFILE
     done
 fi
 
 # Regenerate the hashes for all of the files in the directory.
-echo -e "MD5 Checksums for /home/user/Cyber/sums:\n" > /example/dir/logging/sumCheck.txt
-for i in /example/dir/dir-to-check/*
+SUMCHECKFILE=/home/user/Cyber/sumCheck.txt
+echo -e "MD5 Checksums for /home/user/Cyber/sums:\n" > $SUMCHECKFILE
+for i in /home/user/Cyber/sums/*
 do
-	echo "File Name: " $i >> /example/dir/logging/sumCheck.txt
-        md5sum $i | awk ' { print "MD5 Sum: " $1 } ' >> /example/dir/logging/sumCheck.txt
-	echo "" >> /example/dir/logging/sumCheck.txt
+	echo "File Name: " $i >> $SUMCHECKFILE
+        md5sum $i | awk ' { print "MD5 Sum: " $1 } ' >> $SUMCHECKFILE
+	echo "" >> $SUMCHECKFILE
 done
 
 # If the current hashes don't match the original, an error
 # will be printed to your log file describing the changes.
-if diff "/example/dir/logging/sumCheck.txt "/example/dir/logging/sums.txt" >/dev/null; then
-    echo "No issues: " >> /example/dir/logging/sumLog.txt
-    date >> /example/dir/logging/sumLog.txt
-    echo "" >> /example/dir/logging/sumLog.txt
-    rm /example/dir/logging/sumCheck.txt
-else
-    LOGFILE=/example/dir/logging/sumLog.txt
-    # If log file has not been created, then create it. Otherwise,
-    # append to the already-created log file.
-    if [ ! -f "$LOGFILE" ]; then
-        echo "WARNING: One or more files have been changed" > /example/dir/logging/sumLog.txt
-    else
-        echo "WARNING: One or more files have been changed" > /example/dir/logging/sumLog.txt
-    fi
-    date >> /home/user/Cyber/logs/cronSumLog.txt
-    echo "Changes: " >> /home/user/Cyber/logs/cronSumLog.txt
-    diff "/home/user/Cyber/sumCheck.txt" "/home/user/Cyber/sums.txt" >> /home/user/Cyber/logs/cronSumLog.txt
-    echo "" >> /home/user/Cyber/logs/cronSumLog.txt
+LOGFILE=/home/user/Cyber/logs/cronSumLog.txt
+# If log file has not been created, then create it. Otherwise,
+# append to the already-created log file.
+if [ ! -f "$LOGFILE" ]; then
+    touch $LOGFILE
 fi
-
-# If the current hashes don't match the original, an error
-# will be printed to your log file describing the changes.
-if diff "/home/user/Cyber/sumCheck.txt" "/home/user/Cyber/sums.txt" >/dev/null; then
-    echo "No issues: " >> /home/user/Cyber/logs/cronSumLog.txt
-    date >> /home/user/Cyber/logs/cronSumLog.txt
-    echo "" >> /home/user/Cyber/logs/cronSumLog.txt
+if diff "$SUMCHECKFILE" "$SUMFILE" >/dev/null; then
+    echo "No issues: " >> $LOGFILE
+    date >> $LOGFILE
+    echo "" >> $LOGFILE
     rm sumCheck.txt
 else
-    LOGFILE=/home/user/Cyber/logs/cronSumLog.txt
-    # If log file has not been created, then create it. Otherwise,
-    # append to the already-created log file.
-    if [ ! -f "$LOGFILE" ]; then
-        echo "WARNING: One or more files have been changed" > /home/user/Cyber/logs/cronSumLog.txt
-    else
-        echo "WARNING: One or more files have been changed" > /home/user/Cyber/logs/cronSumLog.txt
-    fi
-    date >> /home/user/Cyber/logs/cronSumLog.txt
-    echo "Changes: " >> /home/user/Cyber/logs/cronSumLog.txt
-    diff "/home/user/Cyber/sumCheck.txt" "/home/user/Cyber/sums.txt" >> /home/user/Cyber/logs/cronSumLog.txt
-    echo "" >> /home/user/Cyber/logs/cronSumLog.txt
+    echo "WARNING: One or more files have been changed" >> $LOGFILE
+    date >> $LOGFILE
+    echo "Changes: " >> $LOGFILE
+    diff "$SUMCHECKFILE" "$SUMFILE" >> $LOGFILE
+    echo "" >> $LOGFILE
 fi
