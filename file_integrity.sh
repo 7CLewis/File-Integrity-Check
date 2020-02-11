5e0c094ce76d3a49ad282c1015f31f45b1aede30
7CLewis <casey.lewis08@gmail.com>
2020-02-10 19:20:38 -0700

    Updated file_integrity.sh
    Modified name for readability.
    
    Generalized directory names to be filled in with the user's desired directory.

--- file_integrity.sh
+++ file_integrity.sh
@@ -5,10 +5,10 @@
 # you should delete the sums.txt file and execute this script immediately
 # after the deletion (You should also check the log file to verify that
 # no other files were unintentionally/maliciously modified before you).
-SUMFILE=/home/user/Cyber/sums.txt
+SUMFILE=/EXAMPLE/DIR/LOGGING/sums.txt
 if [ ! -f "$SUMFILE" ]; then
-    echo -e "MD5 Checksums for /home/user/Cyber/sums:\n" > $SUMFILE
-    for i in /home/user/Cyber/sums/*
+    echo -e "MD5 Checksums for /EXAMPLE/DIR/DIR-TO-VALIDATE:\n" > $SUMFILE
+    for i in /EXAMPLE/DIR/DIR-TO-VALIDATE/*
     do
         echo "File Name: " $i >> $SUMFILE
         md5sum $i | awk ' { print "MD5 Sum: " $1 } ' >> $SUMFILE
@@ -17,9 +17,9 @@ if [ ! -f "$SUMFILE" ]; then
 fi
 
 # Regenerate the hashes for all of the files in the directory.
-SUMCHECKFILE=/home/user/Cyber/sumCheck.txt
-echo -e "MD5 Checksums for /home/user/Cyber/sums:\n" > $SUMCHECKFILE
-for i in /home/user/Cyber/sums/*
+SUMCHECKFILE=/EXAMPLE/DIR/LOGGING/sumCheck.txt
+echo -e "MD5 Checksums for /EXAMPLE/DIR/DIR-TO-VALIDATE:\n" > $SUMCHECKFILE
+for i in /EXAMPLE/DIR/DIR-TO-VALIDATE/*
 do
 	echo "File Name: " $i >> $SUMCHECKFILE
         md5sum $i | awk ' { print "MD5 Sum: " $1 } ' >> $SUMCHECKFILE
@@ -28,7 +28,7 @@ done
 
 # If the current hashes don't match the original, an error
 # will be printed to your log file describing the changes.
-LOGFILE=/home/user/Cyber/logs/cronSumLog.txt
+LOGFILE=EXAMPLE/DIR/LOGGING/cronSumLog.txt
 # If log file has not been created, then create it. Otherwise,
 # append to the already-created log file.
 if [ ! -f "$LOGFILE" ]; then
